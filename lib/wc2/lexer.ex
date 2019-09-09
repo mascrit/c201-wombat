9730ffe6b81655bbdef46588b207e7609411e406
MariMaple <marianita_maple@comunidad.unam.mx>
2019-09-09 00:58:29 -0500

    codigo del lexer (a revisar)

--- lib/wc2/lexer.ex
+++ lib/wc2/lexer.ex
@@ -5,8 +5,41 @@ defmodule Wc2.Lexer do
   @moduledoc """
   Analizador Léxico
   """
-  def hola do
-    'Hola' |> IO.puts
-  end
 
-end
+  @doc """
+  Lee un archivo y devuelve una lista de tokens
+  """
+  @spec lexer(String.t(),integer)::list 
+    def lexer(tokens, numero_linea) do
+      
+      clave=keywords()
+      const=~r(^[0-9]+)
+      espacio=~r(^[ \h]+)
+      linea=~r(^[\n\\|\r\n])
+      cadena_caracter=~r/^"(?:[""\\]|\\.)*"/
+
+      cond do
+    
+        tokens==""->[]
+        Regex.match?(espacio,tokens)->lexer(Regex.replace(espacio,tokens,""),num_linea)
+        Regex.match?(linea,tokens)->lexer(Regex.replace(espacio,tokens,""),num_linea+1)
+        Regex.match?(cadena_caracter,tokens)->string=List.first(Regex.run(cadena_caracter,tokens,[{:capture,:first}]))
+        token={:ident,num_linea,[id]}
+
+      end 
+    end
+    def devuelve_token(token) do
+      case token do 
+        {:type,:kint}->"int"
+        {:kmain}->"main()"
+        {:open_paren}->"("
+        {:close_paren}->")"
+        {:open_brace}->"{"
+        {:kreturn}->"return"
+        {:const, 2}->"2"
+        {:semicolon}->";"
+        {:close_brance}->"}"   
+      
+      end
+                
+    end
