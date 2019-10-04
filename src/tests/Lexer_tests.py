4b68f79edf25424f7697014a3518a6ac99c2d46a
Cristian Romero <mascrit@gmail.com>
2019-10-03 22:56:25 -0500

    Primera version pruebas

---
+++ src/tests/Lexer_tests.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+
+
+class LexerTests(unittest.TestCase):
+
+    def test_ex(self):
+        self.assertEqual('ok', 'ok')
+
+if __name__ == '__main__':
+    unittest.main()
