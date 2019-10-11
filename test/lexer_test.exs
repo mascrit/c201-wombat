e5ffbe7458ee79c434f1a7758587f2ea4947ec47
Cristian Romero <mascrit@gmail.com>
2019-10-10 21:50:08 -0500

    Pruebas lexer, Debe enviarme los tokens, el parser se encarga de los errores e inconsistencias

--- test/lexer_test.exs
+++ test/lexer_test.exs
@@ -1,3 +1,6 @@
+#!/usr/bin/elixir
+# -*- coding: utf-8 -*-
+
 defmodule LexerTest do
   use ExUnit.Case
   doctest Wc2.Lexer
@@ -5,38 +8,101 @@ defmodule LexerTest do
   setup_all do
     {:ok,
      tokens: {:ok, [
-       :kint,
-       :kmain,
-       :open_paren,
-       :close_paren,
-       :open_brace,
-       :kreturn,
-       {:const, 2},
-       :semicolon,
-       :close_brace
-     ]},
+		 :kint,
+		 :kmain,
+		 :open_paren,
+		 :close_paren,
+		 :open_brace,
+		 :kreturn,
+		 {:const, 2},
+		 :semicolon,
+		 :close_brace
+	       ]},
      tokens_0: {:ok, [
-       :kint,
-       :kmain,
-       :open_paren,
-       :close_paren,
-       :open_brace,
-       :kreturn,
-       {:const, 0},
-       :semicolon,
-       :close_brace
-     ]},
+		   :kint,
+		   :kmain,
+		   :open_paren,
+		   :close_paren,
+		   :open_brace,
+		   :kreturn,
+		   {:const, 0},
+		   :semicolon,
+		   :close_brace
+		 ]},
      tokens_100: {:ok,[
-       :kint,
-       :kmain,
-       :open_paren,
-       :close_paren,
-       :open_brace,
-       :kreturn,
-       {:const, 100},
-       :semicolon,
-       :close_brace
-     ]}}
+		     :kint,
+		     :kmain,
+		     :open_paren,
+		     :close_paren,
+		     :open_brace,
+		     :kreturn,
+		     {:const, 100},
+		     :semicolon,
+		     :close_brace
+		   ]},
+     invtokens_paren: {:ok,
+		       [
+			 :kint,
+			 :kmain,
+			 :open_paren,
+			 :open_brace,
+			 :kreturn,
+			 {:const, 0},
+			 :semicolon,
+			 :close_brace
+		       ]},
+     invtokens_m_retval: {:ok,
+			  [
+			    :kint,
+			    :kmain,
+			    :open_paren,
+			    :close_paren,
+			    :open_brace,
+			    :kreturn,
+			    :semicolon,
+			    :close_brace
+			  ]},
+     invtokens_m_brace: {:ok, [
+			    :kint,
+			    :kmain,
+			    :open_paren,
+			    :close_paren,
+			    :open_brace,
+			    :kreturn,
+			    {:const, 0},
+			    :semicolon,
+			  ]},
+     invtokens_m_semicolon: {:ok, [
+				:kint,
+				:kmain,
+				:open_paren,
+				:close_paren,
+				:open_brace,
+				:kreturn,
+				{:const, 0},
+				:close_brace
+			      ]},
+     invtokens_m_space: {:ok,
+			 [
+			    :kint,
+			    :kmain,
+			    :open_paren,
+			    :close_paren,
+			    :identifier,
+			    :close_brace
+			  ]},
+     invtoken_upcase: {:ok,
+		       [
+			    :kint,
+			    :kmain,
+			    :open_paren,
+			    :close_paren,
+			    :open_brace,
+			    :identifier,
+			    {:const, 0},
+			    :close_brace
+			]}
+    }
   end
 
   # test "return 2", state do
@@ -49,7 +115,9 @@ defmodule LexerTest do
   #     assert IO.inspect( Wc2.Lexer.scanner_words(something))
   #   end
   # end
-  
+
+
+  # -- Pruebas Válidas stage_1
   test "multidigit", state do
     code = File.read!("test/stage_1/valid/multi_digit.c")
     with {:ok, something} <- Wc2.Lexer.sanitizer(code) do
@@ -84,12 +152,61 @@ defmodule LexerTest do
       assert Wc2.Lexer.scanner_words(something)==state[:tokens]
     end
   end
-  test "space ", state do
+  test "space", state do
     code = File.read!("test/stage_1/valid/spaces.c")
     with {:ok, something} <- Wc2.Lexer.sanitizer(code) do
       assert Wc2.Lexer.scanner_words(something)==state[:tokens_0]
     end
   end
-  
+
+  # -- Pruebas Inválidas stage_1
+
+  test "missing paren", state do
+    code = File.read!("test/stage_1/invalid/missing_paren.c")
+    
+    with {:ok, something} <- Wc2.Lexer.sanitizer(code) do
+      assert Wc2.Lexer.scanner_words(something)==state[:invtokens_paren]
+    end
+  end
+
+  test "missing retval", state do
+    code = File.read!("test/stage_1/invalid/missing_retval.c")
+    
+    with {:ok, something} <- Wc2.Lexer.sanitizer(code) do
+      assert Wc2.Lexer.scanner_words(something)==state[:invtokens_m_retval]
+    end
+  end
+
+  test "no brace", state do
+    code = File.read!("test/stage_1/invalid/no_brace.c")
+    
+    with {:ok, something} <- Wc2.Lexer.sanitizer(code) do
+      assert Wc2.Lexer.scanner_words(something)==state[:invtokens_m_brace]
+    end
+  end
+
+  test "no semicolon", state do
+     code = File.read!("test/stage_1/invalid/no_semicolon.c")
+    
+    with {:ok, something} <- Wc2.Lexer.sanitizer(code) do
+      assert Wc2.Lexer.scanner_words(something)==state[:invtoken_m_semicolon]
+    end
+  end
+
+  test "no space", state do
+     code = File.read!("test/stage_1/invalid/no_semicolon.c")
+    
+    with {:ok, something} <- Wc2.Lexer.sanitizer(code) do
+      assert Wc2.Lexer.scanner_words(something)==state[:invtoken_m_space]
+    end
+  end
+
+  test "wrong case", state do
+     code = File.read!("test/stage_1/invalid/wrong_case.c")
+    
+    with {:ok, something} <- Wc2.Lexer.sanitizer(code) do
+      assert Wc2.Lexer.scanner_words(something)==state[:invtoken_upcase]
+    end
+  end
   
 end
